d122c1eb2f8a35f1f34f2795c0425f5d1008658f
Huseyin Turkay <compliantuk.uk@gmail.com>
2026-04-15 13:15:04 +0100

    Final Fix: Silo redirects active

--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/blog',
+        has: [{ type: 'query', key: 'slug', value: 'official-renters-rights-act-information-sheet-landlord-guide' }],
+        destination: '/rra/compliance/information-sheet',
+        permanent: true,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
