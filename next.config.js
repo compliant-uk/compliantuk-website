ca894f75490bb4a32c88773fa8d8ad601108af2a
Huseyin Turkay <compliantuk.uk@gmail.com>
2026-04-15 15:01:39 +0100

    Final Fix: Catch homepage slugs

--- next.config.js
+++ next.config.js
@@ -1,22 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'query',
-            key: 'slug',
-            value: 'official-renters-rights-act-information-sheet-landlord-guide',
-          },
-        ],
-        destination: '/rra/compliance/information-sheet',
-        permanent: true,
-      },
-    ]
-  },
-}
-
-module.exports = nextConfig
+async redirects() {
+  return [
+    {
+      source: '/', // Specifically target the homepage
+      has: [{ type: 'query', key: 'slug', value: 'official-renters-rights-act-information-sheet-landlord-guide' }],
+      destination: '/rra/compliance/information-sheet',
+      permanent: true,
+    },
+    {
+      source: '/:path+', // Target all other pages (blog, etc.)
+      has: [{ type: 'query', key: 'slug', value: 'official-renters-rights-act-information-sheet-landlord-guide' }],
+      destination: '/rra/compliance/information-sheet',
+      permanent: true,
+    },
+  ]
+},
